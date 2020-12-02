bff7f78730f84bbfb814018e64769d2f054eb396
Chris Tralie <chris.tralie@gmail.com>
2020-12-02 11:32:19 -0500

    Modifying starter code

--- fractal.cpp
+++ fractal.cpp
@@ -1,25 +1,39 @@
 #include <stdio.h>
 #include "BMP.h"
 
+#define MAX_DEPTH 8
+
 /**
  * Draw a triangle at the specified coordinates
- * @param bmp Reference to the bitmap to which to draw triangle
+ * @param canvas Reference to the bitmap to which to draw triangle
  * @param x0, y0, x1, y1, x2, y2: Coordinates of points
  * @param thickness Thickness of line to draw
  */
-void drawTriangle(BMP* bmp, int x0, int y0, int x1, int y1, int x2, int y2, int thickness) {
-    bmp->plotLine(x0, y0, x1, y1, thickness);
-    bmp->plotLine(x1, y1, x2, y2, thickness);
-    bmp->plotLine(x2, y2, x0, y0, thickness);
+void drawTriangle(BMP* canvas, int x0, int y0, int x1, int y1, int x2, int y2, int thickness) {
+    canvas->plotLine(x0, y0, x1, y1, thickness);
+    canvas->plotLine(x1, y1, x2, y2, thickness);
+    canvas->plotLine(x2, y2, x0, y0, thickness);
+}
+
+/**
+ * Draw a triangle at the specified coordinates
+ * @param canvas Reference to the bitmap to which to draw triangle
+ * @param x0, y0, x1, y1, x2, y2: Coordinates of points
+ * @param depth Depth of recursion
+ */
+void drawSierpinski(BMP* canvas, int x0, int y0, int x1, int y1, int x2, int y2, int depth) {
+    drawTriangle(canvas, x0, y0, x1, y1, x2, y2, 1);
+    // TODO: Fill this in
+    
 }
 
 int main() {
-    int W = 500;
-    int H = 400;
-    BMP bmp(W, H);
-    bmp.clearRect(0xFF, 0xFF, 0xFF, 0xFF);
-    drawTriangle(&bmp, 10, 10, 200, 200, 300, 20, 2);
+    int W = 1000;
+    int H = 1000;
+    BMP canvas(W, H);
+    canvas.clearRect(0xFF, 0xFF, 0xFF, 0xFF);
+    drawSierpinski(&canvas, 10, 10, 500, 990, 1000, 10, 0);
 
-    bmp.write("result.bmp");
+    canvas.write("result.bmp");
     return 0;
 }
